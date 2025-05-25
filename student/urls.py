ce91bdb2113bb009c95781ada913fe0d738eb5d9
rahul <rahulsah7V@123>
2025-05-25 21:26:59 +0530

    student-list

--- student/urls.py
+++ student/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
   
   path('', views.student_list,name="student_list"),
   path('add/', views.add_student,name="add_student"),
+  path('students/<str:slug>/', views.view_student, name='view_student'),
+  path('edit/<str:slug>/', views.edit_student, name='edit_student'),
+  path('delete/<str:slug>/', views.delete_student, name='delete_student'),
+
 
 ]
--- student/views.py
+++ student/views.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.http import HttpResponseForbidden
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import *
 from django.contrib import messages
 # Create your views here.
@@ -10,7 +11,14 @@ def add_student(request):
         last_name = request.POST.get('last_name')
         student_id = request.POST.get('student_id')
         gender = request.POST.get('gender')
-        date_of_birth = request.POST.get('date_of_birth')
+        date_of_birth_str = request.POST.get('date_of_birth')
+        try:
+
+            date_of_birth = datetime.strptime(date_of_birth_str, '%Y-%m-%d').date()
+        except ValueError:
+
+            messages.error(request, "Invalid date format. Use YYYY-MM-DD.")
+            return redirect('add_student', slug=slug)
         student_class = request.POST.get('student_class')
         religion = request.POST.get('religion')
         joining_date = request.POST.get('joining_date')
@@ -63,7 +71,8 @@ def add_student(request):
         )
         #create_notification(request.user, f"Added Student: {student.first_name} {student.last_name}")
         messages.success(request, "Student added Successfully")
-        return render(request, "student_list")
+
+        return redirect('student_list') 
 
   
 
@@ -74,11 +83,88 @@ def add_student(request):
 
 
 def student_list(request):
-    return render(request,"students/students.html")
+    student_list = Student.objects.select_related('parent').all()
+    context = {
+        'student_list' : student_list
+    }
+
+    return render(request,"students/students.html" ,context)
 
 
-def edit_student(request):
-    return render(request,"students/edit-student.html")
+def edit_student(request,slug):
+     student = get_object_or_404(Student,slug=slug)
+     parent = student.parent if hasattr(student,'parent') else None
+     if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        student_id = request.POST.get('student_id')
+        gender = request.POST.get('gender')
+        date_of_birth_str = request.POST.get('date_of_birth')
+        try:
 
-def view_student(request):
-    return render(request,'students/student-details.html')
+            date_of_birth = datetime.strptime(date_of_birth_str, '%Y-%m-%d').date()
+        except ValueError:
+
+            messages.error(request, "Invalid date format. Use YYYY-MM-DD.")
+            return redirect('add_student', slug=slug)
+        student_class = request.POST.get('student_class')
+        religion = request.POST.get('religion')
+        joining_date = request.POST.get('joining_date')
+        mobile_number = request.POST.get('mobile_number')
+        admission_number = request.POST.get('admission_number')
+        section = request.POST.get('section')
+        student_image = request.FILES.get('student_image')
+
+        # Retrieve parent data from the form
+        parent.father_name = request.POST.get('father_name')
+        parent.father_occupation = request.POST.get('father_occupation')
+        parent.father_mobile = request.POST.get('father_mobile')
+        parent.father_email = request.POST.get('father_email')
+        parent.mother_name = request.POST.get('mother_name')
+        parent.mother_occupation = request.POST.get('mother_occupation')
+        parent.mother_mobile = request.POST.get('mother_mobile')
+        parent.mother_email = request.POST.get('mother_email')
+        parent.present_address = request.POST.get('present_address')
+        parent.permanent_address = request.POST.get('permanent_address')
+        parent.save()
+
+        
+        # Save student information
+    
+        student.first_name= first_name
+        student.last_name= last_name
+        student.student_id= student_id
+        student.gender= gender
+        student.date_of_birth= date_of_birth
+        student.student_class= student_class
+        student.religion= religion
+        student.joining_date= joining_date
+        student.mobile_number = mobile_number
+        student.admission_number = admission_number
+        student.section = section
+        student.student_image = student_image
+        student.save()
+
+        #create_notification(request.user, f"Added Student: {student.first_name} {student.last_name}")
+        messages.success(request, "Student added Successfully")
+
+        return redirect('student_list') 
+
+     return render(request,"students/edit-student.html",{'student':student, 'parent' : parent})
+
+
+def view_student(request,slug):
+    student = get_object_or_404(Student,slug = slug)
+    context = {
+        'student' : student
+    }
+    return render(request,'students/student-details.html',context)
+
+def delete_student(request,slug):
+    if request.method == "POST":
+        student = get_object_or_404(Student,slug=slug)
+        student_name = f"{student.first_name} {student.last_name} "
+        student.delete()
+
+        return redirect('student_list')
+    return HttpResponseForbidden()
